0c0287c7c92c5576023f3dc85f1b771c858e8a02
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2021-10-27 22:01:00 +0300

    W4H1

--- src/Week4HomeWork1/src/main/java/com/example/week4homework1/controller/BasketController.java
+++ src/Week4HomeWork1/src/main/java/com/example/week4homework1/controller/BasketController.java
@@ -4,7 +4,12 @@ import com.example.week4homework1.entity.Basket;
 import com.example.week4homework1.model.ProductAddRemoveRequest;
 import com.example.week4homework1.service.BasketService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/basket")
--- src/Week4HomeWork1/src/main/java/com/example/week4homework1/entity/Basket.java
+++ src/Week4HomeWork1/src/main/java/com/example/week4homework1/entity/Basket.java
@@ -1,9 +1,20 @@
 package com.example.week4homework1.entity;
 
-import lombok.*;
-import org.apache.catalina.User;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "basket")
